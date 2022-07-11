231cb32cde4e820db72212b29094831823579b1b
Ferbianto <ferbianto@ai.astra.co.id>
2022-07-11 14:49:20 +0700

    Add project files.

---
+++ src/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AppointmentScheduler.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppointmentScheduler
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
---
+++ src/Models/AppointmentSchedule.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppointmentScheduler.Models
+{
+    public class AppointmentSchedule
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
